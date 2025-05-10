a5aa4b017c601d4ddbce5e077bef96bf1a3de8c1
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:42:08 +0530

    Component Declaration page: This is a functional component that receives props as an argument. In this case, props is typed as an empty object (Props), suggesting that the page component doesn't expect any props to be passed into it.
    The component itself is relatively simple and only serves the purpose of rendering the CreateArticlesPage component.

--- app/dashboard/articles/create/page.tsx
+++ app/dashboard/articles/create/page.tsx
@@ -1,4 +1,11 @@
 import CreateArticlesPage from '@/components/articles/create-articles-page'
 import React from 'react'
 
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    
+  )
+}
 
