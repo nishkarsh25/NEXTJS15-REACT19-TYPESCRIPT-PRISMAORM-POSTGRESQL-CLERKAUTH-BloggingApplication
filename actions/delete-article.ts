8704151cf002aad755bd567dc731d616f6c68d4b
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:33:14 +0530

    Deleting the Article Uses Prisma's delete() method on the articles model.
    It specifies a where clause to locate the article using its unique id.
    
    This is a safe and atomic operation — it ensures only the exact article with the given ID is deleted, and nothing else.

--- actions/delete-article.ts
+++ actions/delete-article.ts
@@ -4,7 +4,10 @@ import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export const deleteArticle = async (articleId: string) => {
-  
+  await prisma.articles.delete({
+    where: { id: articleId }
+  });
+
 
   
 };
