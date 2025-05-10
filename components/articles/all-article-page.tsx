e20d892ae11622cfb5c34e4e3b8dccb020f67ae7
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 12:10:21 +0530

    Fallback Component: NoSearchResults If no matching articles are found, this is shown:
    A centered message with a search icon, heading, and a brief suggestion to try different keywords.
    
    Tailwind classes center the message and give it spacing and visual clarity.

--- components/articles/all-article-page.tsx
+++ components/articles/all-article-page.tsx
@@ -30,7 +30,38 @@ const AllArticlePage: React.FC<AllArticlePageProps> = async ({ articles }) => {
   return (
     <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {articles.map((article) => (
-        
+        <Card
+          key={article.id}
+          className="group relative overflow-hidden translate-all hover:shadow-lg"
+        >
+          <div className="p-6">
+            <div className="relative mb-4 h-48 w-full overflow-hidden rounded-xl">
+              <Image
+                src={article.featuredImage}
+                alt="blog-image"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            {/* Article Content */}
+            <h3 className="text-xl font-semibold">{article.title}</h3>
+            <p className="mt-2 text-sm">{article.category}</p>
+
+            <div className="mt-6 flex items-center justify-between">
+              <div className="flex items-center gap-3">
+                <Avatar>
+                  <AvatarImage src={article.author.imageUrl || ""} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+
+                <span>{article.author.name}</span>
+              </div>
+
+              <div className="text-sm">article.createdAt.toDateString()</div>
+            </div>
+          </div>
+        </Card>
       ))}
     </div>
   );
@@ -38,4 +69,18 @@ const AllArticlePage: React.FC<AllArticlePageProps> = async ({ articles }) => {
 
 export default AllArticlePage;
 
+const NoSearchResults = () => {
+  return (
+    <div className="flex flex-col items-center justify-center p-8 text-center">
+      <div className="mb-4 rounded-full bg-muted p-4">
+        <Search className="h-8" />
+      </div>
 
+      <h1 className="font-bold text-xl ">No result found</h1>
+      <p className="mt-2 text-sm">
+        We could not find any articles matching your search. Try a different
+        keyword or phrase.
+      </p>
+    </div>
+  );
+};
