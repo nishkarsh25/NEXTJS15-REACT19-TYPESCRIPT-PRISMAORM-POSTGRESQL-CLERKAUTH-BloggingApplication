19d17de43011c9d66a3c68172e73fe915d46ae36
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:30:10 +0530

    UI Structure a. Page Header The header includes:
    A title ("All Articles").
    
    An input field (ArticleSearchInput) for searching articles.
    
    b. Suspense for Asynchronous Data
    The Suspense component is used around AllArticlePage, which is responsible for rendering the list of articles.
    
    While the articles are loading, the AllArticlesPageSkeleton (a loading placeholder) is displayed to the user.
    
    c. Pagination
    Pagination controls are provided at the bottom:
    
    Previous Page: A button that links to the previous page of articles. The button is disabled if the current page is the first page.
    
    Page Numbers: A series of buttons representing the page numbers. The current page is highlighted using a different button style.
    
    Next Page: A button that links to the next page of articles. This is disabled if the current page is the last page.

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -24,8 +24,61 @@ const page: React.FC<SearchPageProps> = async ({ searchParams }) => {
 
   const totalPages = Math.ceil(total / ITEMS_PER_PAGE);
 
-  
-};
+  return (
+    <div className="min-h-screen bg-background">
+      <main className="container mx-auto px-4 py-12 sm:px-6 lg:text-5xl">
+        {/* Page Header */}
+        <div className="mb-12 space-y-6 text-center">
+          <h1 className="text-4xl font-bold sm:text-5xl">All Articles</h1>
 
+          {/* Search Bar */}
+          <ArticleSearchInput />
+        </div>
+
+        {/* All Article card */}
+        <Suspense fallback={<AllArticlesPageSkeleton />}>
+          <AllArticlePage articles={articles} />
+        </Suspense>
+
+        {/* pagination */}
+        <div className="mt-12 flex justify-center gap-2">
+          <Link href={`?search=${searchText}&page=${currentPage - 1}`} passHref>
+            <Button disabled={currentPage === 1} variant={"ghost"} size={"sm"}>
+              {" "}
+              ← Prev
+            </Button>
+          </Link>
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <Link
+              key={index}
+              href={`?search=${searchText}&page=${index + 1}`}
+              passHref
+            >
+              <Button
+                variant={`${
+                  currentPage === index + 1 ? "destructive" : "ghost"
+                }`}
+                size={"sm"}
+              >
+                {index + 1}
+              </Button>
+            </Link>
+          ))}
+
+          <Link href={`?search=${searchText}&page=${currentPage + 1}`} passHref>
+            <Button
+              disabled={currentPage === totalPages}
+              variant={"ghost"}
+              size={"sm"}
+            >
+              {" "}
+              Next →
+            </Button>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 
