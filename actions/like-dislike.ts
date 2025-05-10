289c893335a9cd6f624d199aa8c9012c0496a617
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:41:53 +0530

    Main Logic: likeDislikeToggle Function

--- actions/like-dislike.ts
+++ actions/like-dislike.ts
@@ -4,4 +4,15 @@ import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
+export const likeDislikeToggle = async (articleId: string) => {
+  
 
+  
+
+ 
+
+  
+
+  
+  
+};
