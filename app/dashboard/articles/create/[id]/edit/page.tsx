a302e0ed4a95fc79a7e634d671a3dbc3c8f658f5
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:43:29 +0530

    Fetching Article Data The id of the article is extracted from the params object, and a Prisma query is used to fetch the article from the database. The query uses the prisma.articles.findUnique method to retrieve the article with the given id from the articles table in the database.
    await prisma.articles.findUnique({ where: { id } }): This query looks for a single article based on the id passed as a parameter.

--- app/dashboard/articles/create/[id]/edit/page.tsx
+++ app/dashboard/articles/create/[id]/edit/page.tsx
@@ -7,7 +7,10 @@ type EditArticleParams = {
 };
 
 const page: React.FC<EditArticleParams> = async ({ params }) => {
-  
+  const id = (await params).id;
+  const article = await prisma.articles.findUnique({
+    where: { id },
+  });
 
   
   
