f99bfeea559c90d0a841e833fc091d7991cff901
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:01:41 +0530

    Imports searchAction: This is an action that is likely defined elsewhere (possibly in @/actions/search) and is called when the form is submitted. It handles the search query, possibly by querying a database or performing a search operation.
    Input: This is a custom input field component from the UI library, used to accept user input for the search query.
    
    Search: A search icon imported from lucide-react to display inside the search input field for better UX.
    
    useSearchParams: This is a Next.js hook from next/navigation that allows access to the query parameters in the URL. It is used to pre-fill the search input with the current query parameter when the page is loaded.

--- components/home/header/search-input.tsx
+++ components/home/header/search-input.tsx
@@ -1,4 +1,8 @@
 "use client";
-
+import { searchAction } from "@/actions/search";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 
