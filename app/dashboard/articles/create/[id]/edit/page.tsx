0a29a4e02dc8e33b8fe7b32cb763b37fd4f19d70
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:43:13 +0530

    Type Definition EditArticleParams: This type defines the expected structure of the props that the component will receive. In this case, it specifies that the params will be a promise that resolves to an object containing the id of the article as a string.

--- app/dashboard/articles/create/[id]/edit/page.tsx
+++ app/dashboard/articles/create/[id]/edit/page.tsx
@@ -2,7 +2,9 @@ import EditArticlePage from "@/components/articles/edit-article-page";
 import { prisma } from "@/lib/prisma";
 import React from "react";
 
-
+type EditArticleParams = {
+  params: Promise<{ id: string }>;
+};
 
 
 
