b858f47d9e0b890c338733903efc4ee148205eb2
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:11:14 +0530

    Cloudinary Configuration Cloudinary is configured using environment variables for cloud_name, api_key, and api_secret.
    This setup enables uploading media to your Cloudinary account securely.

--- actions/create-article.ts
+++ actions/create-article.ts
@@ -6,7 +6,11 @@ import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
-
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 
