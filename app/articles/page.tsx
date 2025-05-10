6de5ec8ba74ad2f7fbb6accadbc2a77ba55d2a64
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:26:48 +0530

    Imports Components:
    AllArticlePage: Displays the list of articles on the page.
    
    ArticleSearchInput: A component that allows users to search for articles.
    
    Button: A UI component for buttons, used for pagination and other interactions.
    
    Card: A UI component used to display article information, especially for loading skeletons.
    
    Skeleton: A placeholder component to simulate loading content, enhancing UX while data is being fetched.
    
    Library Imports:
    
    Link from Next.js is used for client-side navigation to different pages (pagination links).
    
    fetchArticleByQuery: A custom helper function for fetching articles based on search and pagination criteria.

---
+++ app/articles/page.tsx
@@ -0,0 +1,16 @@
+import AllArticlePage from "@/components/articles/all-article-page";
+import ArticleSearchInput from "@/components/articles/article-search-input";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { fetchArticleByQuery } from "@/lib/query/fetch-article-by-query";
+import Link from "next/link";
+import React, { Suspense } from "react";
+
+
+
+
+
+
+
+
