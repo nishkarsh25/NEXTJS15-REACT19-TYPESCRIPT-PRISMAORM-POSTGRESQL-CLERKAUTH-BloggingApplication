87de805468b8cdb867a2de2b618e98003c406d4c
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:51:15 +0530

    Fetching a list of matching articles This query retrieves multiple article records (findMany) from the database.
    The filtering condition is defined using an OR clause:
    
    It checks whether the title or the category field contains the searchText.
    
    The mode: "insensitive" option ensures the search is case-insensitive.
    
    It also includes details of the author of each article:
    
    Specifically, it selects the author's name, email, and image URL.
    
    The skip and take parameters are applied:
    
    skip skips a given number of records (for pagination).
    
    take limits the number of records fetched.

--- lib/query/fetch-article-by-query.ts
+++ lib/query/fetch-article-by-query.ts
@@ -6,7 +6,27 @@ export const fetchArticleByQuery = async (
   take: number
 ) => {
   const [articles, total] = await prisma.$transaction([
-    
+    prisma.articles.findMany({
+      where: {
+        OR: [
+          { title: { contains: searchText, mode: "insensitive" } },
+          { category: { contains: searchText, mode: "insensitive" } },
+        ],
+      },
+
+      include: {
+        author: {
+          select: {
+            name: true,
+            imageUrl: true,
+            email: true,
+          },
+        },
+      },
+
+      skip: skip,
+      take: take,
+    }),
 
     
   ]);
