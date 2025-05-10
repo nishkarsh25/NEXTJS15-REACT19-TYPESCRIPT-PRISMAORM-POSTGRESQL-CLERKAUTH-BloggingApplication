039159bb635541d5564b9657d29c4a676619a8ab
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:21:20 +0530

    The Layout Component Declaration This is a server component (as indicated by the use of await at the top level) that wraps around children routes or components.
    It receives a children prop, which represents the part of the app that this layout will render around (e.g., nested routes, pages).

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,5 +2,13 @@ import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  
+
+ 
+
+  
+  
+};
 
 
