90bbf0f6e96f38b7c238443399a34e4f49e32190
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:15:41 +0530

    Imports Overview React: Essential for building the component.
    cn: A utility function (likely from clsx or a custom helper) used to conditionally merge Tailwind CSS classes cleanly.
    
    Card: A UI component that likely provides a styled container or card layout.
    
    Link: From Next.js, used for client-side navigation to article detail pages.
    
    Image: Optimized image handling from Next.js that enables fast loading, lazy loading, and responsive images.
    
    Avatar and its subcomponents: Used to render the author’s profile picture, with fallback initials if the image isn’t available.
    
    prisma: A database ORM (Object Relational Mapper). It enables easy querying and manipulation of data from a database.

---
+++ components/home/top-articles.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Card } from "../ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { prisma } from "@/lib/prisma";
+
+
+
+
