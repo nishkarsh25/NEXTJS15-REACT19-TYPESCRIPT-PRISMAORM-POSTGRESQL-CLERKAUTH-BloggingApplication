64d65315a8122c5b4018f37ac5d43c8b54d0f054
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:27:22 +0530

    Constants and Variables ITEMS_PER_PAGE: The number of articles to display per page. It is set to 3.
    searchText: Extracted from the searchParams to filter articles based on user input.
    
    currentPage: Represents the current page number for pagination, defaulting to page 1 if not provided.
    
    skip and take: Used for pagination. skip defines how many articles to skip (based on the current page), and take specifies how many articles to fetch (based on ITEMS_PER_PAGE).

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -11,8 +11,21 @@ type SearchPageProps = {
   searchParams: Promise<{ search?: string; page?: string }>;
 };
 
+const ITEMS_PER_PAGE = 3;
 
+const page: React.FC<SearchPageProps> = async ({ searchParams }) => {
+  const searchText = (await searchParams).search || "";
+  const currentPage = Number((await searchParams).page) || 1;
 
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE;
+  const take = ITEMS_PER_PAGE;
+
+  
+
+  
+
+  
+};
 
 
 
