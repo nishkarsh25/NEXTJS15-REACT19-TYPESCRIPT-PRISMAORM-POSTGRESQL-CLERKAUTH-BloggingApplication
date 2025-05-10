67e17feab54bb0099547e77d5f8688d446c086b8
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:34:11 +0530

    Component Declaration page is a functional React component with asynchronous behavior. It is defined as React.FC<ArticleDetailPageProps>, where ArticleDetailPageProps describes the expected structure of props that the component receives.
    The ArticleDetailPageProps type defines that the params prop is a promise that resolves to an object containing an id (the ID of the article).

---
+++ app/articles/[id]/page.tsx
@@ -0,0 +1,19 @@
+import ArticleDetailPage from "@/components/articles/article-detail-page";
+import { prisma } from "@/lib/prisma";
+import React from "react";
+
+type ArticleDetailPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+const page: React.FC<ArticleDetailPageProps> = async ({ params }) => {
+  const id = (await params).id;
+
+  
+
+  
+
+  
+};
+
+
