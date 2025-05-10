c63342820a2dfb1621163ee699e525db3578cfe3
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:21:47 +0530

    Fetching the Authenticated User The function currentUser() gets the currently signed-in Clerk user (on the server).
    If no user is authenticated (i.e., user is null or undefined), the layout returns null. That means nothing is rendered, effectively blocking unauthenticated access.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -3,7 +3,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  
+  const user = await currentUser();
+  if (!user) return null;
 
  
 
