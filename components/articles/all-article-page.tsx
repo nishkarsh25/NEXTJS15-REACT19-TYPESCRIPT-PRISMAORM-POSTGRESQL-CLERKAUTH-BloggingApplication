974253b58b15ca8a7bbe27a2f199214243d5ad4c
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 23:49:54 +0530

    All bugs in developement have been fixed:

--- components/articles/all-article-page.tsx
+++ components/articles/all-article-page.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { fetchArticleByQuery } from "@/lib/query/fetch-article-by-query";
 import { Search } from "lucide-react";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 type AllArticlePageProps = {
   articles: Prisma.ArticlesGetPayload<{
@@ -21,8 +22,6 @@ type AllArticlePageProps = {
 };
 
 const AllArticlePage: React.FC<AllArticlePageProps> = async ({ articles }) => {
-  
-
   if (articles.length <= 0) {
     return <NoSearchResults />;
   }
@@ -30,38 +29,39 @@ const AllArticlePage: React.FC<AllArticlePageProps> = async ({ articles }) => {
   return (
     <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {articles.map((article) => (
-        <Card
-          key={article.id}
-          className="group relative overflow-hidden translate-all hover:shadow-lg"
-        >
-          <div className="p-6">
-            <div className="relative mb-4 h-48 w-full overflow-hidden rounded-xl">
-              <Image
-                src={article.featuredImage}
-                alt="blog-image"
-                fill
-                className="object-cover"
-              />
-            </div>
-
-            {/* Article Content */}
-            <h3 className="text-xl font-semibold">{article.title}</h3>
-            <p className="mt-2 text-sm">{article.category}</p>
-
-            <div className="mt-6 flex items-center justify-between">
-              <div className="flex items-center gap-3">
-                <Avatar>
-                  <AvatarImage src={article.author.imageUrl || ""} />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-
-                <span>{article.author.name}</span>
+        <Link key={article.id} href={`/articles/${article.id}`}>
+          <Card className="group relative overflow-hidden translate-all hover:shadow-lg">
+            <div className="p-6">
+              <div className="relative mb-4 h-48 w-full overflow-hidden rounded-xl">
+                <Image
+                  src={article.featuredImage}
+                  alt="blog-image"
+                  fill
+                  className="object-cover"
+                />
               </div>
 
-              <div className="text-sm">article.createdAt.toDateString()</div>
+              {/* Article Content */}
+              <h3 className="text-xl font-semibold">{article.title}</h3>
+              <p className="mt-2 text-sm">{article.category}</p>
+
+              <div className="mt-6 flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    <AvatarImage src={article.author.imageUrl || ""} />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+
+                  <span>{article.author.name}</span>
+                </div>
+
+                <div className="text-sm">
+                  {article.createdAt.toDateString()}
+                </div>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        </Link>
       ))}
     </div>
   );
