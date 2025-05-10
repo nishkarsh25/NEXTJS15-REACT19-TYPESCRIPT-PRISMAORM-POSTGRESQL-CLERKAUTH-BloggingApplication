bdbb024be3b925f80df39677caa0a9c7355d5442
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 16:35:29 +0530

    Heading Section Contains a title "Blog Dashboard" and a subtitle "Manage Your content and analytics".
    There’s a Link component wrapped around a Button to navigate to the article creation page. The button is styled using a custom Button component, and the PlusCircle icon is added to represent the action visually.

--- components/dashboard/blog-dashboard.tsx
+++ components/dashboard/blog-dashboard.tsx
@@ -29,7 +29,17 @@ const BlogDashBoard = async (props: Props) => {
   ]);
   return (
     <main className="flex-1 p-4 md:p-8">
-      
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <h1 className="font-bold text-2xl">Blog Dashboard</h1>
+          <p>Manage Your content and analytics</p>
+        </div>
+
+        <Link href={"/dashboard/articles/create"}>
+          <PlusCircle className="h-4 w-4" />
+          <Button>New article</Button>
+        </Link>
+      </div>
 
       
 
