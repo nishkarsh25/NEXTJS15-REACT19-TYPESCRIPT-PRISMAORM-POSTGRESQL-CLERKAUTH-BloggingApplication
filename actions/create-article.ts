62ab069e983c475fc08a0ce2574348b8ce468b84
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:11:36 +0530

    Custom Type for Form Error State A TypeScript type defines a structured format for potential errors returned to the client.
    It allows errors to be grouped by form field (title, category, featuredImage, content) and a general formErrors array for broader issues.

--- actions/create-article.ts
+++ actions/create-article.ts
@@ -18,7 +18,15 @@ const createArticleSchema = z.object({
   content: z.string().min(10),
 });
 
-
+type CreateArticlesFormState = {
+  errors: {
+    title?: string[];
+    category?: string[];
+    featuredImage?: string[];
+    content?: string[];
+    formErrors?: string[];
+  };
+};
 
 
 
