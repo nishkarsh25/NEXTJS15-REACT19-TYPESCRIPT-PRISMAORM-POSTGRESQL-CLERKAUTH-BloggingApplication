93085ad7a8985d3de194645f31ca9fb40c563feb
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 17:23:53 +0530

    Imports Navbar, HeroSection, TopArticles, and BlogFooter are custom React components used to build various sections of the homepage UI.
    Button is a styled UI button component.
    
    Link from Next.js is used for client-side routing.
    
    Suspense from React is used for lazy-loading components with a fallback UI.
    
    AllArticlesPageSkeleton is a placeholder (skeleton UI) shown while TopArticles loads.

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import BlogFooter from "@/components/home/blog-footer";
+import Navbar from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/hero-section";
+import TopArticles from "@/components/home/top-articles";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import { AllArticlesPageSkeleton } from "../articles/page";
+
+
+
+
