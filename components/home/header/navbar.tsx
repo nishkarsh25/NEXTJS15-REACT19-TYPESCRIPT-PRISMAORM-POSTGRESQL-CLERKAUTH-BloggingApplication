4b721c098b16db8375896cd4cde81d8415c11697
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 16:58:19 +0530

    Left Section (Logo) This section contains the ByteCode logo with gradient text using Tailwind CSS classes (bg-gradient-to-r, text-transparent, etc.).
    Clicking the logo redirects the user to the homepage (/).

--- components/home/header/navbar.tsx
+++ components/home/header/navbar.tsx
@@ -23,7 +23,17 @@ const Navbar = (props: Props) => {
     <div className="sticky top-0 z-50 w-full  border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          
+          {/* LeftSection */}
+          <div className="flex items-center gap-8">
+            <Link href="/" className="flex items-center space-x-2">
+              <span className="font-bold text-2xl">
+                <span className="bg-gradient-to-r from-purple-600 to bg-indigo-600 dark:from-purple-400 dark:to-indigo-400 bg-clip-text text-transparent">
+                  Byte
+                </span>
+                <span className="text-foreground">Code</span>
+              </span>
+            </Link>
+          </div>
 
          
 
