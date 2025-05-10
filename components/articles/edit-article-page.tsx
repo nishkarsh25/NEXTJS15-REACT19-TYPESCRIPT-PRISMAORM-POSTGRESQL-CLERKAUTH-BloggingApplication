9c0e12a82bb32f417c22b8f876506c5817204e77
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 16:14:27 +0530

    Imports React + Hooks: For handling form submissions and component state.
    UI Components: Card, Input, Label, Button used for styling and layout.
    
    next/dynamic: Used to load the ReactQuill component on the client only, avoiding SSR-related errors.
    
    Quill CSS: The snow.css stylesheet provides default styles for the rich text editor.
    
    editArticle action: The main server function responsible for updating an article in the backend.
    
    Articles from Prisma: Defines the article prop type structure.
    
    next/image: Imported but not used here; instead, a native <img> tag is used for preview.

--- components/articles/edit-article-page.tsx
+++ components/articles/edit-article-page.tsx
@@ -1,4 +1,20 @@
 "use client";
+import React, {
+  FormEvent,
+  startTransition,
+  useActionState,
+  useState,
+} from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import dynamic from "next/dynamic";
+import { Button } from "../ui/button";
+import "react-quill-new/dist/quill.snow.css";
+import { createArticle } from "@/actions/create-article";
+import { Articles } from "@prisma/client";
+import Image from "next/image";
+import { editArticle } from "@/actions/edit-article";
 
 
 
