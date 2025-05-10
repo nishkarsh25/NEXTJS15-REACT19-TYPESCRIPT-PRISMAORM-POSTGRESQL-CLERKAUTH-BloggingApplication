9d59f05b5babbf8223a1b93a268649532c462411
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 12:08:27 +0530

    Type Definition: AllArticlePageProps This defines the expected props of the component:
    articles: An array of Articles records, where each includes:
    
    The author details (name, email, and imageUrl) using Prisma’s typing system.
    
    This ensures TypeScript enforces the correct structure when the component is used.

--- components/articles/all-article-page.tsx
+++ components/articles/all-article-page.tsx
@@ -6,6 +6,19 @@ import { fetchArticleByQuery } from "@/lib/query/fetch-article-by-query";
 import { Search } from "lucide-react";
 import { Prisma } from "@prisma/client";
 
+type AllArticlePageProps = {
+  articles: Prisma.ArticlesGetPayload<{
+    include: {
+      author: {
+        select: {
+          name: true;
+          email: true;
+          imageUrl: true;
+        };
+      };
+    };
+  }>[];
+};
 
 
 
