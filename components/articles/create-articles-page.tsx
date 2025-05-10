75fb94507a613ebd3688258ce7151242cd643943
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 22:04:31 +0530

    Styling fixes:

--- components/articles/create-articles-page.tsx
+++ components/articles/create-articles-page.tsx
@@ -59,14 +59,14 @@ const CreateArticlesPage = (props: Props) => {
             <div className="space-y-2">
               <Label>Category</Label>
               <select
-                className="flex h-10 w-full rounded-md"
-                name="category"
                 id="category"
+                name="category"
+                className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
               >
-                <option value="">Select category</option>
+                <option value="">Select Category</option>
                 <option value="technology">Technology</option>
                 <option value="programming">Programming</option>
-                <option value="web-developmentt">Web Development</option>
+                <option value="web-development">Web Development</option>
               </select>
               {formState.errors.category && (
                 <span className="text-red-600 text-sm">
