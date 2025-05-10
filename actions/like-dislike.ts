a2926badcab3911f2a5db6f7aaff4d492a3d5434
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:42:02 +0530

    Retrieve userId from auth() The auth() function returns the user’s authentication details. Here, it extracts the userId to identify the logged-in user.
    If the userId is not found (i.e., the user is not logged in), the function throws an error, indicating the user must be logged in to like or dislike an article.

--- actions/like-dislike.ts
+++ actions/like-dislike.ts
@@ -5,7 +5,11 @@ import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 export const likeDislikeToggle = async (articleId: string) => {
-  
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("You must logged in to like an article");
+  }
 
   
 
