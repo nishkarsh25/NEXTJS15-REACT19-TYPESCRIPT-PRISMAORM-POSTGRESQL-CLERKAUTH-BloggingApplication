9a5cccae09a0867db805437f7238336632b4ae67
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 11:26:00 +0530

    Schema Definition Using Zod A validation schema is defined using zod to ensure the incoming comment (body) is not empty.
    It enforces that the body field (i.e., the actual comment text) must be a non-empty string (at least 1 character).

--- actions/create-comment.ts
+++ actions/create-comment.ts
@@ -1,7 +1,12 @@
 "use server";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
 
-
-
+const createCommentSchema = z.object({
+  body: z.string().min(1),
+});
 
 
 
