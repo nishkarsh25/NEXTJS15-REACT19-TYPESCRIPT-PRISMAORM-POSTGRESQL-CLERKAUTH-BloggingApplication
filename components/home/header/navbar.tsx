22dab966a29e89b4dbbf7767bf6ca6dfcc18badf
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-10 16:58:09 +0530

    Navbar Layout The layout of the navbar is divided into three main sections:

--- components/home/header/navbar.tsx
+++ components/home/header/navbar.tsx
@@ -19,7 +19,23 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  
+  return (
+    <div className="sticky top-0 z-50 w-full  border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          
+
+         
+
+          
+
+          
+        </div>
+      </div>
+
+      
+    </div>
+  );
 };
 
 
